7f84b5ec50b4f645c96a38660f16e7938bfeb6b8
murat uçar <murat_ua@outlook.com>
2022-02-25 11:35:53 +0300

    UserLogin is added.

--- TweetBook/Contracts/V1/Requests/UserLoginRequest.cs
+++ TweetBook/Contracts/V1/Requests/UserLoginRequest.cs
@@ -1,10 +1,12 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
 
 namespace TweetBook.Contracts.V1.Requests
 {
-    public class UserRegistrationRequest
+    public class UserLoginRequest
     {
-        [EmailAddress]
         public string Email { get; set; }
         public string Password { get; set; }
     }
--- TweetBook/Services/IdentityService.cs
+++ TweetBook/Services/IdentityService.cs
@@ -23,8 +23,37 @@ namespace TweetBook.Services
             _jwtSettings = jwtSettings;
         }
 
+        public async Task<AuthenticationResult> LoginAsync(string email, string password)
+        {
+
+            var user = await _userManager.FindByEmailAsync(email);
+
+            if (user != null)
+            {
+                return new AuthenticationResult
+                {
+                    Errors = new[] { "User/passwprd combination is wrong" }
+                };
+            }
+
+            var userHasValidPassword = await _userManager.CheckPasswordAsync(user, password);
+
+            if (!userHasValidPassword)
+            {
+                return new AuthenticationResult
+                {
+                    Errors = new[] { "User does not exits" }
+                };
+            }
+
+
+            return GenerateAuthenticationResultForUser(user);
+        }
+
         public async Task<AuthenticationResult> RegisterAsync(string email, string password)
         {
+
+
             var existingUser = await _userManager.FindByEmailAsync(email);
 
             if (existingUser != null)
@@ -76,7 +105,34 @@ namespace TweetBook.Services
                 Success = true,
                 Token = tokenHandler.WriteToken(token)
             };
-           
+
+        }
+
+        private AuthenticationResult GenerateAuthenticationResultForUser(IdentityUser newUser)
+        {
+            var tokenHandler = new JwtSecurityTokenHandler();
+            var key = Encoding.ASCII.GetBytes(_jwtSettings.Secret);
+            var tokenDescriptor = new SecurityTokenDescriptor
+            {
+                Subject = new ClaimsIdentity(new[]
+                {
+                new Claim(JwtRegisteredClaimNames.Sub,newUser.Email),
+                new Claim(JwtRegisteredClaimNames.Jti,Guid.NewGuid().ToString()),
+                new Claim(JwtRegisteredClaimNames.Email,newUser.Email),
+                new Claim("id",newUser.Id)
+                }),
+                Expires = DateTime.UtcNow.AddHours(2),
+                SigningCredentials = new SigningCredentials(new SymmetricSecurityKey(key), SecurityAlgorithms.HmacSha256Signature)
+
+            };
+
+            var token = tokenHandler.CreateToken(tokenDescriptor);
+
+            return new AuthenticationResult
+            {
+                Success = true,
+                Token = tokenHandler.WriteToken(token)
+            };
         }
     }
 }
